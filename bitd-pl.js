4eac608f73449814cbc08e1e5f97a273ccb61119
Axotte <axotte@gmail.com>
2023-05-28 17:09:50 +0200

    Update bitd-pl.js

--- bitd-pl.js
+++ bitd-pl.js
@@ -1,16 +1,9 @@
 Hooks.once('init', () => { 
-	if(typeof Babele !== 'undefined') 
-	{ 
-		
-		console-log("Babele Loaded");
-		Babele.get().register
-		({
-			module: 'bitd-pl',
-			lang: 'pl-pl',
-			dir: 'compendium'
+	if(typeof Babele !== 'undefined') { 
+	Babele.get().register({
+		module: 'bitd-pl',
+		lang: 'pl-pl',
+		dir: 'compendium'
 		});
-	} else {
-		console.log("++++++++ Moduł Babele nie zostal aktywowany ++++++++");
-			}
 	}
 });
